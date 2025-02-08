eb295955311ba362a80bd91ab34820540964d4a8
ChristianB020 <125928748+ChristianB020@users.noreply.github.com>
2025-02-08 00:50:15 -0500

    add components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 
 import './App.css';
+import Header from './Components/Header';
+import Nav from './Components/Nav';
+import Footer from './Components/Footer'
+import Main from './Components/Main'
 
 function App() {
   return (
-    <div className="App">
-  
-    </div>
+    <>
+
+
+
+
+<Header></Header>
+      <Nav></Nav>
+
+
+      <main>
+
+      </main>
+
+
+      <footer>
+
+      </footer>
+    </>
   );
 }
 
